1f8351c53fa3a4ab9af0e63b4c8beff1f1bb5077
Diane Vail <dsval3@gmail.com>
2024-12-15 15:38:17 -0500

    webdev-js-intro-01

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+const one = 1;
+const more = 75;
+const one = 76;//Uncaught SyntaxError: Identifier 'one' has already been declared
+                //This means that the variable "one " has already been assigned and can not be reassigned.
+let user = "diane";
+console.log(user);
+
+user = "Dan";
+console.log(Dan);//favicon.ico:1 
+        
+        
+//Failed to load resource: the server responded with a status of 404 (Not Found)
